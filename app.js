860d97da2472cd4fe4ae901174d313fc5343aa7d
muddge <root@localhost.localdomain>
2020-04-01 03:34:15 -0400

    latest commit

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-vare app = express();
+var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
